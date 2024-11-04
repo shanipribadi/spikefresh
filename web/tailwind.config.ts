e76c9058be5323aaf8409995c0f426037bf276bb
Shani Hadiyanto <shanipribadi@gmail.com>
2024-11-04 09:45:52 +0700

    feat: darkTheme override

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "tailwindcss";
 import daisyui from "npm:daisyui";
+import {nord as light, sunset as dark} from "npm:daisyui/src/theming/themes.js";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  daisyui: {
+    themes: [{light: light}, {dark: dark}],
+  },
+  darkMode: ["selector", '[data-theme="dark"]'],
   plugins: [
     // deno-lint-ignore no-explicit-any
     daisyui as any,
